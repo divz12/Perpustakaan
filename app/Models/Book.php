f7cf0901c227137175bdb090c7669a65e0781832
divz12 <divasativa99@gmail.com>
2024-02-19 20:43:20 +0700

    first commit

---
+++ app/Models/Book.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Book extends Model
+{
+    use HasFactory;
+    use Sluggable;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'book_code',
+        'title',
+        'cover',
+        'slug',
+        'status',
+        'deleted_at',
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'book_category', 'book_id', 'category_id');
+    }
+
+    
+}
